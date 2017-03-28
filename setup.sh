402f35bee89ef9eb374b88aa8c0f9c429678472c
Nicolas Coderre <ncoderre@zynga.com>
2017-03-28 11:09:22 -0700

    initial commit of basic setup with custom playcanvas engine as submodule

---
+++ setup.sh
@@ -0,0 +1,11 @@
+git submodule init
+git submodule update
+
+
+
+sudo npm install -g bower
+sudo npm install -g typescript
+sudo npm install typings --global
+
+echo *** please run the standard build instructions for play canvas in client/lib/engine
+
